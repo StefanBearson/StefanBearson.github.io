8ec1c354921c08e8bb457879db8f246de51994bb
Stefan Björnson <43110881+StefanBearson@users.noreply.github.com>
2020-02-20 21:27:57 +0100

    course progress

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 console.log(
-  "%c --::Version:  %c1.0.004%c ::-- ©2020 Stefan Björnson ",
-  "color: chocolate; font-weight:bolder; background-color: black; font-size:15px",
-  "color: white; font-weight:bolder; background-color: black; font-size:15px",
-  "color: chocolate; font-weight:bolder; background-color: black; font-size:15px"
+  '%c --::Version:  %c1.0.005%c ::-- ©2020 Stefan Björnson ',
+  'color: chocolate; font-weight:bolder; background-color: black; font-size:15px',
+  'color: white; font-weight:bolder; background-color: black; font-size:15px',
+  'color: chocolate; font-weight:bolder; background-color: black; font-size:15px'
 );
 
 const printEmplymentList = () => {
-  let output = document.getElementById("emplymentslist");
-  let html = "";
+  let output = document.getElementById('emplymentslist');
+  let html = '';
 
   employment.map(e => {
     html += "<div class='listitem employment'>";
-    html += "<h4>" + e.jobtitle + "</h4>";
-    html += "<h5>" + e.company + "</h5>";
-    html += "<p>Anställd: " + e.start + " till " + e.end + "</p>";
-    html += "<p>" + e.description + "</p>";
-    html += "</div>";
+    html += '<h4>' + e.jobtitle + '</h4>';
+    html += '<h5>' + e.company + '</h5>';
+    html += '<p>Anställd: ' + e.start + ' till ' + e.end + '</p>';
+    html += '<p>' + e.description + '</p>';
+    html += '</div>';
   });
 
   output.innerHTML = html;
 };
 
 const printYHList = () => {
-  let output = document.getElementById("yhlist");
-  let html = "";
+  let output = document.getElementById('yhlist');
+  let html = '';
 
   courses.map(e => {
-    if (e.place == "EC Utbildning AB Helsingborg") {
-      html += "<a href=" + e.courseLink + "><div class='listitem'>";
+    if (e.place == 'EC Utbildning AB Helsingborg') {
+      html += '<a href=' + e.courseLink + "><div class='listitem'>";
       html +=
-        "<h5>" +
+        '<h5>' +
         e.course +
-        "</h5><p>" +
+        '</h5><p>' +
         e.description +
         "</p><p class='time'> betyg: " +
         e.grade +
-        "</p>";
-      html += "</div></a>";
+        '</p>';
+      html += '</div></a>';
     }
   });
 
@@ -44,31 +44,31 @@ const printYHList = () => {
 };
 
 const printudemyList = () => {
-  let output = document.getElementById("udemylist");
-  let html = "";
+  let output = document.getElementById('udemylist');
+  let html = '';
 
   courses.map(e => {
-    if (e.place != "EC Utbildning AB Helsingborg") {
-      html += "<a href=" + e.courseLink + ">";
+    if (e.place != 'EC Utbildning AB Helsingborg') {
+      html += '<a href=' + e.courseLink + '>';
       html +=
         "<div class='listitem' style='background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) " +
         e.completion +
-        "%, rgba(223,198,198,1) " +
+        '%, rgba(223,198,198,1) ' +
         e.completion +
-        "%, rgba(223,198,198,1) " +
+        '%, rgba(223,198,198,1) ' +
         e.completion +
         "%);'><h5>" +
         e.course +
-        "</h5>" +
+        '</h5>' +
         "<p class='info'>" +
         e.description +
-        "</p>" +
+        '</p>' +
         "<p class='time'>" +
         e.length +
-        "</p>" +
-        "</div>";
+        '</p>' +
+        '</div>';
 
-      html += "</a>";
+      html += '</a>';
     }
   });
 
@@ -79,68 +79,68 @@ printEmplymentList();
 printudemyList();
 printYHList();
 
-const udemyInfo = document.getElementById("udemyinfo");
-const udmeyAction = document.getElementById("udemyaction");
+const udemyInfo = document.getElementById('udemyinfo');
+const udmeyAction = document.getElementById('udemyaction');
 
-udmeyAction.addEventListener("click", function() {
-  if (udemyInfo.classList.contains("hidden")) {
-    udemyInfo.classList.replace("hidden", "notHidden");
+udmeyAction.addEventListener('click', function() {
+  if (udemyInfo.classList.contains('hidden')) {
+    udemyInfo.classList.replace('hidden', 'notHidden');
   } else {
-    udemyInfo.classList.replace("notHidden", "hidden");
+    udemyInfo.classList.replace('notHidden', 'hidden');
   }
 });
 
-const ecInfo = document.getElementById("ecinfo");
-const ecAction = document.getElementById("ecaction");
+const ecInfo = document.getElementById('ecinfo');
+const ecAction = document.getElementById('ecaction');
 
-ecAction.addEventListener("click", function() {
-  if (ecInfo.classList.contains("hidden")) {
-    ecInfo.classList.replace("hidden", "notHidden");
+ecAction.addEventListener('click', function() {
+  if (ecInfo.classList.contains('hidden')) {
+    ecInfo.classList.replace('hidden', 'notHidden');
   } else {
-    ecInfo.classList.replace("notHidden", "hidden");
+    ecInfo.classList.replace('notHidden', 'hidden');
   }
 });
 
-const eccoursesInfo = document.getElementById("eccoursesinfo");
-const eccoursesAction = document.getElementById("eccoursesaction");
+const eccoursesInfo = document.getElementById('eccoursesinfo');
+const eccoursesAction = document.getElementById('eccoursesaction');
 
-eccoursesAction.addEventListener("click", function() {
-  if (eccoursesInfo.classList.contains("hidden")) {
-    eccoursesInfo.classList.replace("hidden", "notHidden");
+eccoursesAction.addEventListener('click', function() {
+  if (eccoursesInfo.classList.contains('hidden')) {
+    eccoursesInfo.classList.replace('hidden', 'notHidden');
   } else {
-    eccoursesInfo.classList.replace("notHidden", "hidden");
+    eccoursesInfo.classList.replace('notHidden', 'hidden');
   }
 });
 
-const udemycoursesInfo = document.getElementById("udemycoursesinfo");
-const udemycoursesAction = document.getElementById("udemycoursesaction");
+const udemycoursesInfo = document.getElementById('udemycoursesinfo');
+const udemycoursesAction = document.getElementById('udemycoursesaction');
 
-udemycoursesAction.addEventListener("click", function() {
-  if (udemycoursesInfo.classList.contains("hidden")) {
-    udemycoursesInfo.classList.replace("hidden", "notHidden");
+udemycoursesAction.addEventListener('click', function() {
+  if (udemycoursesInfo.classList.contains('hidden')) {
+    udemycoursesInfo.classList.replace('hidden', 'notHidden');
   } else {
-    udemycoursesInfo.classList.replace("notHidden", "hidden");
+    udemycoursesInfo.classList.replace('notHidden', 'hidden');
   }
 });
 
-const aboutmeInfo = document.getElementById("aboutmeinfo");
-const aboutmeAction = document.getElementById("aboutmeaction");
+const aboutmeInfo = document.getElementById('aboutmeinfo');
+const aboutmeAction = document.getElementById('aboutmeaction');
 
-aboutmeAction.addEventListener("click", function() {
-  if (aboutmeInfo.classList.contains("hidden")) {
-    aboutmeInfo.classList.replace("hidden", "notHidden");
+aboutmeAction.addEventListener('click', function() {
+  if (aboutmeInfo.classList.contains('hidden')) {
+    aboutmeInfo.classList.replace('hidden', 'notHidden');
   } else {
-    aboutmeInfo.classList.replace("notHidden", "hidden");
+    aboutmeInfo.classList.replace('notHidden', 'hidden');
   }
 });
 
-const employmentInfo = document.getElementById("employmentinfo");
-const employmentAction = document.getElementById("employmentaction");
+const employmentInfo = document.getElementById('employmentinfo');
+const employmentAction = document.getElementById('employmentaction');
 
-employmentAction.addEventListener("click", function() {
-  if (employmentInfo.classList.contains("hidden")) {
-    employmentInfo.classList.replace("hidden", "notHidden");
+employmentAction.addEventListener('click', function() {
+  if (employmentInfo.classList.contains('hidden')) {
+    employmentInfo.classList.replace('hidden', 'notHidden');
   } else {
-    employmentInfo.classList.replace("notHidden", "hidden");
+    employmentInfo.classList.replace('notHidden', 'hidden');
   }
 });
