83d40cc6f3f9fdd6cd72ee4fbd9e9ae749ebea78
Stefan Björnson <43110881+StefanBearson@users.noreply.github.com>
2020-03-11 15:38:41 +0100

    course progress

--- data.js
+++ data.js
@@ -348,7 +348,7 @@ let courses = [
       'En omfattande kurs i C#! Man får lära sig WPF, Databashantering, Linq, och en del spelprogrammering.',
     teacher: 'Denis Panjuta',
     teacherLink: 'https://www.udemy.com/user/denispanjuta/',
-    completion: '81',
+    completion: '86',
     grade: ''
   },
   {
